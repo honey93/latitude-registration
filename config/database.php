74264bdb5a6e880fb071b3f7f5be1bc069e69cfd
Honey Thakuria <thakuria.honey@gmail.com>
2017-01-23 06:55:06 +0530

    Database Config added

--- config/database.php
+++ config/database.php
@@ -1,5 +1,22 @@
 <?php
 
+
+
+$url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+  $host = $url["host"];
+  $username = $url["user"];
+  $password = $url["pass"];
+  $database = substr($url["path"], 1);
+
+
+
+   // $host = "localhost";
+   // $username = "root";
+   // $password = "";
+   // $database = "around";
+
+
 return [
 
     /*
@@ -54,11 +71,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'localhost'),
+            'host' => $host,
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix' => '',
